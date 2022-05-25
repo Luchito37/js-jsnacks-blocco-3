df4c99c92dee06d6f0095f723c2e8a472a7807e6
Luchito37 <lucamatteotti01@gmail.com>
2022-05-25 15:41:57 +0200

    push di  correzzione per domenico

--- snack2/scriptsecond.js
+++ snack2/scriptsecond.js
@@ -3,3 +3,20 @@ Snack2
 Fai inserire un numero, che chiameremo N, all’utente. Genera N array, ognuno 
 formato da 10 numeri casuali da 1 a 100. Ogni volta che ne crei uno, stampalo.
 */
+
+
+
+const askNumber = prompt("Inserisci un numero :")
+
+let arrayFirst = []
+
+let randomArray = askNumber * (arrayFirst)
+
+console.log(randomArray)
+
+for(let i = 0; i < 10; i++){
+
+    casualArrayContent = arrayFirst[Math.round(Math.random()*10)+1];
+
+}
+
